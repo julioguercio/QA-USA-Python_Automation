10b98a70e3dbf4794ad98b0e6acfeb04be2571cf
guercjx <julioguercios@gmail.com>
2026-09-13 21:33:26 -0400

    Repository was created Virtual environments were created and activated. New file and constants were added and prepared order to verify no errors. Sample data was inputted into data.py

--- data.py
+++ data.py
@@ -1,1 +1,7 @@
-# File with constants
+URBAN_ROUTES_URL = "https://cnt-083cdd9c-ca6f-42ec-950a-21ac5718aff7.containerhub.tripleten-services.com"
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
